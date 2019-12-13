df51896f587680ce2828e8ece042dea65a75eb9d
DeAndre Johnson <dre@phreshr.com>
2019-12-13 11:38:58 +0800

    add api route

--- api/server.mjs
+++ api/server.mjs
@@ -1,6 +1,11 @@
 import express from "express";
+import recipeRouter from "../recipes/recipe-router.mjs";
 
 const server = express();
+
+server.use(express.json());
+
+server.use("/api/recipes", recipeRouter);
 
 server.get("/", (req, res) => {
   res.send("<h1>Node DB4 Challenge</h1>");
