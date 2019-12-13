bb87d12b31672b083d55c894453b526e0d66035b
DeAndre Johnson <dre@phreshr.com>
2019-12-13 08:52:58 +0800

    update knex config

--- knexfile.js
+++ knexfile.js
@@ -3,8 +3,15 @@
 module.exports = {
   development: {
     client: "sqlite3",
+    useNullAsDefault: true,
     connection: {
       filename: "./data/recipes.sqlite3"
+    },
+    migrations: {
+      directory: "./data/migrations"
+    },
+    seeds: {
+      directory: "./data/seeds"
     }
   }
 };
